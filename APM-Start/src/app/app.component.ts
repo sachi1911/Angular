531305b20316bb02793c3fbf7a1b24df2ef2cda0
sachi1911 <66085594+sachi1911@users.noreply.github.com>
2021-09-21 01:35:11 +0530

    module-12

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ProductService } from './products/product.service';
+
 
 @Component({
   selector: 'pm-root',
@@ -19,7 +19,7 @@ import { ProductService } from './products/product.service';
       </div>
   </div>
   `,
-    providers:[ ProductService ]
+    
 })
 export class AppComponent {
   pageTitle :String = 'Acme Product Management';
